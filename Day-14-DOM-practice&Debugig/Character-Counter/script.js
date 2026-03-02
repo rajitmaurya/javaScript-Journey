5e9b1ea5556bb8c636191de2702495ce752c50df
rajit8674 <rajitmaurya8318@gmail.com>
2026-03-02 18:56:44 +0530

    character-couter

--- Day-14-DOM-practice&Debugig/Character-Counter/script.js
+++ Day-14-DOM-practice&Debugig/Character-Counter/script.js
@@ -1,10 +1,32 @@
 let input = document.querySelector("#textInput");
 let countDisplay = document.querySelector("#count");
 
+let maxLimit = 20;
+
 input.addEventListener("input", function () {
   let text = input.value;
-  countDisplay.textContent = text.length;
+
+  // Limit apply
+  if (text.length > maxLimit) {
+    input.value = text.slice(0, maxLimit);
+  }
+
+  countDisplay.textContent = input.value.length;
+  countDisplay.textContent = input.value.length + " / " + maxLimit;
+
+  // Color change
+  if (input.value.length === maxLimit) {
+    countDisplay.style.color = "red";
+  } else {
+    countDisplay.style.color = "black";
+  }
 });
 
-let words = text.trim().split(" ").filter(Boolean);
-console.log(words.length);
+let warning = document.querySelector("#warning");
+
+if (input.value.length === maxLimit) {
+  warning.textContent = "Character limit reached!";
+  warning.style.color = "red";
+} else {
+  warning.textContent = "";
+}
