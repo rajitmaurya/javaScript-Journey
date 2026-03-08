48adcaf21b75b47a417a77fe93d1b955695aa355
rajit8674 <rajitmaurya8318@gmail.com>
2026-03-08 19:02:51 +0530

    practice

---
+++ Day-16-Modern-JavaScript/ArrowFunction/practice.js
@@ -0,0 +1,13 @@
+//Multiply Function
+const multiply = (a, b) => {
+  return a * b;
+};
+
+console.log(multiply(4, 5));
+
+//short Arrow
+// const multiply = (a, b) => a * b;
+// 
+// console.log(multiply(4,5));
+
+
