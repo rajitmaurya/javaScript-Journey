1cf9c399498e16b18d753d64f30220a429f45bf4
rajit8674 <rajitmaurya8318@gmail.com>
2026-03-08 19:04:55 +0530

    sqaure

--- Day-16-Modern-JavaScript/ArrowFunction/practice.js
+++ Day-16-Modern-JavaScript/ArrowFunction/practice.js
@@ -11,3 +11,15 @@ console.log(multiply(4, 5));
 // console.log(multiply(4,5));
 
 
+// Square Function
+const square = (num) => {
+  return num * num;
+};
+
+console.log(square(6));
+
+
+//Short Arrow
+// const square = num => num * num;
+// 
+// console.log(square(6));
