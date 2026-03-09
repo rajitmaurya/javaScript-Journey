223aaffa7af76d1e3e93888bc322eae4c86c7d86
rajit8674 <rajitmaurya8318@gmail.com>
2026-03-09 19:52:05 +0530

    practice-of desturucturing

---
+++ Day-16-Modern-JavaScript/Destructuring/practice.js
@@ -0,0 +1,13 @@
+// object
+let user = {
+  name: "Rajit",
+  age: 22,
+  city: "Delhi"
+};
+
+// destructuring
+let { name, age, city } = user;
+
+console.log(name);
+console.log(age);
+console.log(city);
