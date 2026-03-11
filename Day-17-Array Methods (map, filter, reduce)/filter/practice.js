5b2fb2a52bea6530cd66d79df70a13f51ae40943
rajit8674 <rajitmaurya8318@gmail.com>
2026-03-11 16:25:23 +0530

    practice of filetr

---
+++ Day-17-Array Methods (map, filter, reduce)/filter/practice.js
@@ -0,0 +1,10 @@
+// Numbers greater than 30
+
+const numbers = [10,25,30,45,50];
+
+const result = numbers.filter(num => num > 30);
+
+console.log(result); //[45,50]
+
+
+//filter() condition true hone wale elements return karta hai.
