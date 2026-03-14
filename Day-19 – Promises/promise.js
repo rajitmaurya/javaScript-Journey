064c70f3342d4632574afe74ae8c761379daa238
rajit8674 <rajitmaurya8318@gmail.com>
2026-03-14 19:32:53 +0530

    what is promises

---
+++ Day-19 – Promises/promise.js
@@ -0,0 +1,13 @@
+// Promise kya hota hai?
+// 
+// Promise = future me milne wala result
+// 
+// Example real life:
+// 
+// Food order kiya
+// → Wait
+// → Food mil gaya (Resolved)
+// → Food nahi mila (Rejected)
+
+
+
