9f8b43cba6dca7d6d408673f9add17af528d3b2f
rajit8674 <rajitmaurya8318@gmail.com>
2026-03-16 15:49:19 +0530

    delay function

--- Day-20-Async-Await/practice.js
+++ Day-20-Async-Await/practice.js
@@ -5,3 +5,21 @@ async function greet() {
 }
 
 greet().then(result => console.log(result));//Hello JavaScript
+
+
+// 2. Delay function (2 seconds)
+// Goal: 2 sec baad print Loading finished
+function delay() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve("Loading finished");
+    }, 2000);
+  });
+}
+
+async function run() {
+  let result = await delay();
+  console.log(result);
+}
+
+run();
