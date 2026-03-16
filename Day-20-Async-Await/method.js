dc9ba9cf1883233f006ab325ba2054af698e95eb
rajit8674 <rajitmaurya8318@gmail.com>
2026-03-16 13:57:13 +0530

    async function

---
+++ Day-20-Async-Await/method.js
@@ -0,0 +1,19 @@
+// async kya hota hai?
+// async function automatically Promise return karta hai
+
+
+//EXAMPLE
+async function greet() {
+  return "Hello Rajit";
+}
+
+greet().then(result => console.log(result));// hello rajit 
+
+
+
+//await kya hota hai?
+// await promise ka result wait karke return karta hai
+
+
+
+
