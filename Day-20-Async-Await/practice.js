c19224056ce0c166709bcce872752690a9f5c482
rajit8674 <rajitmaurya8318@gmail.com>
2026-03-17 22:27:31 +0530

    practice of async await

--- Day-20-Async-Await/practice.js
+++ Day-20-Async-Await/practice.js
@@ -22,4 +22,62 @@ async function run() {
   console.log(result);
 }
 
-run();
+run(); 
+
+
+
+//3 .Multiple await (Sequential)
+function getUser() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve("User loaded");
+    }, 1000);
+  });
+}
+
+function getOrders() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve("Orders loaded");
+    }, 1000);
+  });
+}
+
+async function loadData() {
+  let user = await getUser();
+  console.log(user);
+
+  let orders = await getOrders();
+  console.log(orders);
+}
+
+loadData(); ////await sequential execution karata hai.
+
+
+
+
+//Error Handling (try/catch)
+function getData() {
+  return new Promise((resolve, reject) => {
+    reject("Something went wrong");
+  });
+}
+
+async function fetchData() {
+  try {
+    let data = await getData();
+    console.log(data);
+  } catch (error) {
+    console.log("Error:", error);
+  }
+}
+
+fetchData();//Error: Something went wrong
+
+
+
+
+// async-function promise return karta
+// await-promise complete hone tak wait
+// try/catch-error handle
+
